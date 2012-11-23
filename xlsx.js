7dcacddcb875f87d0112cafa57515df007c75027
Michael Pickelbauer <reg.mikepee@gmail.com>
2012-11-23 17:45:38 +0100

    Fix styles map to work with multiple worksheets

--- xlsx.js
+++ xlsx.js
@@ -114,11 +114,12 @@ function xlsx(file) { 'use strict'; // v2.0.0
 				+ (file.created || new Date()).toISOString() + '</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">' + (file.modified || new Date()).toISOString() + '</dcterms:modified></cp:coreProperties>');
 		//}
 		//{ Content dependent
+            styles = [,];
 			w = file.worksheets.length;
 			while (w--) { // Generate worksheet (gather sharedStrings), and possibly table files, then generate entries for constant files below
 				id = w + 1;
 				//{ Generate sheetX.xml in var s
-					worksheet = file.worksheets[w]; data = worksheet.data; styles = [,];
+					worksheet = file.worksheets[w]; data = worksheet.data;
 					s = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">'
 						+ '<dimension ref="A1:' + alphabet(data[0].length - 1) + data.length + '"/><sheetViews><sheetView ' + (w === file.activeWorksheet ? 'tabSelected="1" ' : '')
 						+ ' workbookViewId="0"/></sheetViews><sheetFormatPr defaultRowHeight="15" x14ac:dyDescent="0.25"/><sheetData>';
